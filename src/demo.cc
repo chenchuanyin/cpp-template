4b0da90a4adb60e2b687a5d89dadbaa256836348
chenchuanyin <chenchuanyinuestc@gmail.com>
2017-11-13 18:16:40 +0800

    添加abseil-cpp，benchmark等依赖

--- src/demo.cc
+++ src/demo.cc
@@ -1,11 +1,35 @@
-#include <glog/logging.h>
+#include "absl/algorithm/algorithm.h"
+#include "absl/algorithm/container.h"
+#include "absl/base/call_once.h"
+#include "absl/base/config.h"
+#include "absl/memory/memory.h"
+#include "absl/synchronization/blocking_counter.h"
+#include "absl/synchronization/internal/thread_pool.h"
+#include "absl/synchronization/mutex.h"
+#include "glog/logging.h"
 
-void bar(int x) { CHECK_EQ(x, 1); }
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
-void foo(int x) { bar(x + 1); }
+void func(int v) { std::cout << "v:" << v << std::endl; }
 
 int main(int argc, char **argv) {
-  google::InstallFailureSignalHandler();
-  foo(1);
+  google::InitGoogleLogging(argv[0]);
+
+  FLAGS_log_dir = "./log";
+  LOG(INFO) << "hi demo";
+
+  absl::Mutex mutex;
+  absl::MutexLock lock(mutex);
+  absl::BlockingCounter bc;
+  absl::synchronization_internal::ThreadPool pool(4);
+  while (1) {
+    ::sleep(1);
+    pool.Schedule([]() {
+      LOG(INFO) << "1";
+      std::cout << "1\n";
+    });
+  }
   return 0;
 }
