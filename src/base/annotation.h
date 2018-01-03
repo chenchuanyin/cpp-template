7c50ec246e26d0763147c8eeff2b48fc2cdfe408
chenchuanyin <chenchuanyinuestc@gmail.com>
2018-01-03 14:01:16 +0800

    add file .clang_complete

---
+++ src/base/annotation.h
@@ -0,0 +1,8 @@
+#ifndef BASE_ANNOTATION_H__
+#define BASE_ANNOTATION_H__
+
+#define __IN
+#define __OUT
+#define __INOUT
+
+#endif // BASE_ANNOTATION_H__
--- src/demo.cc
+++ src/demo.cc
@@ -1,3 +1,5 @@
+#include "base/annotation.h"
+
 #include "absl/algorithm/algorithm.h"
 #include "absl/algorithm/container.h"
 #include "absl/base/call_once.h"
@@ -8,28 +10,23 @@
 #include "glog/logging.h"
 #include "glog/raw_logging.h"
 
+#include "gtest/gtest.h"
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
-void func(int v) { std::cout << "v:" << v << std::endl; }
-
 int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
-
   FLAGS_log_dir = "./log";
   LOG(INFO) << "hi demo";
 
   absl::Mutex mutex;
   absl::MutexLock lock(&mutex);
-
   absl::synchronization_internal::ThreadPool pool(4);
   while (1) {
     ::sleep(1);
-    pool.Schedule([]() {
-      LOG(INFO) << "1";
-      std::cout << "1\n";
-    });
+    pool.Schedule([]() { LOG(INFO) << "1"; });
   }
   return 0;
 }
